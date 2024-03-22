d62d21fb7f714ededd4c1018d46b6125d6df37d2
DigitalZeus <65643207+DigitalZeus@users.noreply.github.com>
2024-03-22 09:00:31 +0530

    'once again'

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
 export const createError = (status, message) => {
     const err = new Error();
     err.status = status;
-    err.message = message;
+    err.message = message || undefined;
     return err;
   };
